618dff50b5a749d564e3e83a870c7e55b299d1d9
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2025-01-30 18:36:43 +0100

    Change back build script

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -e
 git config --global --add safe.directory /__w/Telemetrix4RpiPico/Telemetrix4RpiPico # fix for cmake not reading git stuff
-# mkdir build2/
-# ls
+mkdir build2/
+ls
+cd build2
+# Sometimes the debug build reports more errors than Release
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+mv Telemetrix4RpiPico_combined.uf2 Telemetrix4RpiPico.uf2
+cd ..
+# rm -rf build2
+# mkdir build2
 # cd build2
-# # Sometimes the debug build reports more errors than Release
+# Build for the artifacts:
 # cmake -DCMAKE_BUILD_TYPE=Debug ..
 # make
-# cd ..
-# rm -rf build2
-mkdir build2
-cd build2
-# Build for the artifacts:
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-make
 sha256sum * || true
